77782da8c6cebba7d8c695737dfc429e5851713b
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2020-03-28 21:27:10 -0600

    Formatting

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ void fillMap(
 
 // -------------------------------------------------
 void fillMapBackwords(
+  //for alphabets that are coming backwords
   std::map<char, int, greater <char> > &temp_map,
   string              str
 ) {
@@ -35,20 +36,23 @@ string shiftToRight(
   int                 intKey
 ) {
   std::map<char,int>::iterator it;
-  string result;
+  string                       result;
 
   for(int i = 0; i < str.length(); i++) {
 
     it = temp_map.find(str[i]);
+
     if(
       //check if we found our char
       it != temp_map.end()
     ) {
       int pos = it->second;
+
       if(
         // check if we havent pass the size of the map
         it->second + intKey < temp_map.size()
       ) {
+
         while(it != temp_map.end()){
           if(
             it->second == pos+intKey
@@ -58,6 +62,7 @@ string shiftToRight(
           }
           it++;
         }
+
       } else {
         int intPos = (intKey + it->second) % temp_map.size();
         if(intPos == 0) {
@@ -73,8 +78,10 @@ string shiftToRight(
           }
           it++;
         }
+
       }
     } else {
+      // keep the char as it was if not included in the alphabet
       result += str[i];
     }
   }
@@ -84,11 +91,11 @@ string shiftToRight(
 
 // -------------------------------------------------
 string shiftToRightBackwords(
-  // this shift is for maps that come with descending order,
-  //basically does the same but due to how maps work need to do another function
+  // this shift is for maps that come with descending order, basically does the
+  // same but due to how maps work needs to be another function
   std::map<char, int, greater <char> > &temp_map,
-  string              str,
-  int                 intKey
+  string                               str,
+  int                                  intKey
 ) {
   std::map<char,int>::iterator it;
   string result;
@@ -96,15 +103,18 @@ string shiftToRightBackwords(
   for(int i = 0; i < str.length(); i++) {
 
     it = temp_map.find(str[i]);
+
     if(
       //check if we found our char
       it != temp_map.end()
     ) {
+
       int pos = it->second;
       if(
         // check if we havent pass the size of the map
         it->second + intKey < temp_map.size()
       ) {
+
         while(it != temp_map.end()){
           if(
             it->second == pos+intKey
@@ -114,12 +124,14 @@ string shiftToRightBackwords(
           }
           it++;
         }
+
       } else {
         int intPos = (intKey + it->second) % temp_map.size();
         if(intPos == 0) {
           intPos += temp_map.size();
         }
         it = temp_map.begin();
+
         while(it != temp_map.end()) {
           if(
             it->second == intPos
@@ -129,8 +141,10 @@ string shiftToRightBackwords(
           }
           it++;
         }
+
       }
     } else {
+      // keep the char as it was if not included in the alphabet
       result += str[i];
     }
   }
@@ -165,7 +179,7 @@ string decryptShiftCipher(
 ){
 
   std::map<char, int, greater <char> > Alphabet;
-  string              strDecrypt;
+  string                               strDecrypt;
 
   fillMapBackwords(Alphabet, strAlphabet);
   strDecrypt = shiftToRightBackwords(
@@ -183,16 +197,16 @@ string useShiftCipher() {
   string strKeyword;
   int    intKey;
 
-  cout << "=================================" << endl;
-  cout << "Enter your Alphabet" << endl;
+  cout << "================================="  << endl;
+  cout << "Enter your Alphabet"                << endl;
   getline(cin, strAlphabet);
-  cout << "Enter your Keyword" << endl;
+  cout << "Enter your Keyword"                 << endl;
   getline(cin, strKeyword);
-  cout << "Enter the Key" << endl;
+  cout << "Enter the Key"                      << endl;
   cin  >> intKey;
-  cout << "=================================" << endl;
+  cout << "================================="  << endl;
   cout << "Do you want to Encrypt or Decrypt?" << endl;
-  cin >> option;
+  cin  >> option;
   cin.ignore();
 
   if(
@@ -204,7 +218,7 @@ string useShiftCipher() {
   ) {
     return decryptShiftCipher(strAlphabet, strKeyword, intKey);
   } else {
-    return "Could not understand";
+    return "Could not understand " << option;
   }
 }
 
@@ -214,16 +228,17 @@ int main() {
   int    intOption;
 
   cout << "=================================" << endl;
-  cout << "What Cypher do you want to do?" << endl;
-  cout << "1. Encrypt Shift Cipher" << endl;
-  cin >> intOption;
+  cout << "What Cypher do you want to do?"    << endl;
+  cout << "1. Shift Cipher"                   << endl;
+  cout << "2. Substitution Cipher"            << endl;
+  cin  >> intOption;
   cin.ignore();
 
   if(intOption == 1) {
     result = useShiftCipher();
   }
-
-  cout << result << endl;
+  cout << "================================="  << endl;
+  cout << result                               << endl;
 
   return 0;
 }
